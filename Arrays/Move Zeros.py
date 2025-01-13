29b2a72f7b2b4a6fcb38422d8884c062ef91a2c1
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-01-13 19:49:24 +0530

    Update Move Zeros.py

--- Arrays/Move Zeros.py
+++ Arrays/Move Zeros.py
@@ -1,1 +1,10 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        position = 0
+        for num in nums:
+            if num != 0:  
+                nums[position] = num  
+                position += 1  
 
+        for i in range(position, len(nums)):
+            nums[i] = 0
