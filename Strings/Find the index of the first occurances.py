3dad9c205d550a932029ca6abaa38ea460068f8c
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-01-14 23:00:12 +0530

    Update Find the index of the first occurances.py

--- Strings/Find the index of the first occurances.py
+++ Strings/Find the index of the first occurances.py
@@ -1,1 +1,13 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        haystack_len = len(haystack)
+        needle_len = len(needle)
 
+        if needle_len == 0:
+            return 0
+
+        for i in range(haystack_len - needle_len + 1):
+            if haystack[i:i + needle_len] == needle:
+                return i
+
+        return -1
