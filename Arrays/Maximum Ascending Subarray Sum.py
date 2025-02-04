f2fe7515bb5d68b20e3ffac42c4de0131203e5b8
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-02-04 19:08:27 +0530

    Update Maximum Ascending Subarray Sum.py

--- Arrays/Maximum Ascending Subarray Sum.py
+++ Arrays/Maximum Ascending Subarray Sum.py
@@ -1,1 +1,10 @@
-
+class Solution(object):
+    def maxAscendingSum(self, nums):
+        ms=cs=nums[0]
+        for i in range(1,len(nums)):
+            if nums[i]>nums[i-1]:
+                cs+=nums[i]
+            else:
+                ms=max(ms,cs)
+                cs=nums[i]
+        return max(ms,cs)
