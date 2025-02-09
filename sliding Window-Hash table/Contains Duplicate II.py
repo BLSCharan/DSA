7d78d8660601cf26fd20c06a74bccf128ca13659
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-02-09 23:31:47 +0530

    Update Contains Duplicate II.py

--- sliding Window-Hash table/Contains Duplicate II.py
+++ sliding Window-Hash table/Contains Duplicate II.py
@@ -1,1 +1,8 @@
-
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        h={}
+        for i in range(len(nums)):
+            if nums[i] in h and abs(i-h[nums[i]])<=k:
+                return True
+            h[nums[i]]=i
+        return False
