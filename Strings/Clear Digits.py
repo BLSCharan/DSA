2ea43998c5aceef7ccc72073cd0c15fe406a2a76
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-02-10 21:52:57 +0530

    Update Clear Digits.py

--- Strings/Clear Digits.py
+++ Strings/Clear Digits.py
@@ -1,1 +1,10 @@
-
+class Solution(object):
+    def clearDigits(self, s):
+        r=""
+        for c in s:
+            if c.isdigit():
+                if r:
+                    r=r[:-1]
+            else:
+                r+=c
+        return r
