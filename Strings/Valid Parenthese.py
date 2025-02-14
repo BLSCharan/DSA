97f4007dd19728e47b87c40155b02909854e3b2b
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-02-14 21:54:31 +0530

    Update Valid Parenthese.py

--- Strings/Valid Parenthese.py
+++ Strings/Valid Parenthese.py
@@ -1,1 +1,12 @@
-
+class Solution(object):
+    def isValid(self, s):
+        st=[]
+        m={')':'(','}':'{',']':'['}
+        for char in s:
+            if char in m:
+                te=st.pop() if st else '#'
+                if m[char]!=te:
+                    return False
+            else:
+                st.append(char)
+        return not st
