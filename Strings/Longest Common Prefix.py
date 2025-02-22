183cbcf2299071e61b087c53f10a7c4f40a7b4ca
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-02-22 21:30:48 +0530

    Update Longest Common Prefix.py

--- Strings/Longest Common Prefix.py
+++ Strings/Longest Common Prefix.py
@@ -1,1 +1,8 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        pre=strs[0]
+        for i in strs:
+            while not i.startswith(pre):
+                pre=pre[:-1]
+        return pre
 
