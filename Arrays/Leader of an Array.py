a9432edb17621cbc03aa58e1c6af954f56011588
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-03-05 20:44:25 +0530

    Update Leader of an Array.py

--- Arrays/Leader of an Array.py
+++ Arrays/Leader of an Array.py
@@ -1,1 +1,12 @@
+n = int(input())
+arr = list(map(int, input().split()))
+for i in range(n):
+    is_largest = True
+    for j in range(i + 1, n):
+        if arr[j] >= arr[i]:
+            is_largest = False
+            break
+    if is_largest:
+        print(arr[i], end=" ")
+print()
 
