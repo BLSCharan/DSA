afe93e1fd987ac361ed6e380a4f778666f262137
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-03-08 10:43:50 +0530

    Update Wordle.py

--- Strings/Wordle.py
+++ Strings/Wordle.py
@@ -1,1 +1,11 @@
-
+t=int(input())
+for i in range(t):
+    s=input()
+    t=input()
+    m=[]
+    for i in range(len(s)):
+        if s[i]==t[i]:
+            m.append("G")
+        else:
+            m.append("B")
+    print("".join(m))
