e137a049830a7a3ebf8299badc05c360af581e4f
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-03-08 10:30:15 +0530

    Update Minimum Recolors to Get K Consecutive Black Blocks.py

--- sliding Window-Hash table/Minimum Recolors to Get K Consecutive Black Blocks.py
+++ sliding Window-Hash table/Minimum Recolors to Get K Consecutive Black Blocks.py
@@ -1,1 +1,14 @@
+class Solution(object):
+    def minimumRecolors(self, blocks, k):
+        count_white = blocks[:k].count('W')
+        min_white = count_white
+        for i in range(k, len(blocks)):
+            if blocks[i] == 'W':
+                count_white += 1 
+            if blocks[i - k] == 'W':
+                count_white -= 1 
+            
+            min_white = min(min_white, count_white)
 
+        return min_white  
+        
