8006c99242b42b04b1fa4bfd0ce974900a6e5643
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-03-09 14:15:16 +0530

    Update Different Consecutive Characters.py

--- Strings/Different Consecutive Characters.py
+++ Strings/Different Consecutive Characters.py
@@ -1,1 +1,11 @@
+t = int(input())
+for _ in range(t):
+    n = int(input()) 
+    s = input().strip()
+    op = 0 
+    for i in range(n - 1):
+        if s[i] == s[i + 1]: 
+            op+= 1
+    
+    print(op)
 
