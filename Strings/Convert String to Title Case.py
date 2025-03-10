43ff323b1c6b74caf9ced35cf357878fecbc3db3
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-03-10 21:37:33 +0530

    Update Convert String to Title Case.py

--- Strings/Convert String to Title Case.py
+++ Strings/Convert String to Title Case.py
@@ -1,1 +1,15 @@
+T = int(input())
+
+for _ in range(T):
+    S = input().strip()
+    words = S.split()
+    result = []
+
+    for word in words:
+        if word.isupper():  
+            result.append(word)
+        else:
+            result.append(word.capitalize())
+
+    print(" ".join(result))  
 
