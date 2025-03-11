c8357465599dd27a6c14c572918f040694b2be27
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-03-11 21:08:27 +0530

    Update Third Maximum Number.py

--- Arrays/Third Maximum Number.py
+++ Arrays/Third Maximum Number.py
@@ -1,1 +1,9 @@
-
+class Solution(object):
+    def thirdMax(self, nums):
+        nums=set(nums)
+        if len(nums)>=3:
+            nums.remove(max(nums))
+            nums.remove(max(nums))
+            return max(nums)
+        else:
+            return max(nums)
