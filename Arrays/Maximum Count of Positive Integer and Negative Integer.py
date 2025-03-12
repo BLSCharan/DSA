6dee48f2a52a1efecdde12ffb4a7d6e6fd03878e
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-03-12 19:48:47 +0530

    Update Maximum Count of Positive Integer and Negative Integer.py

--- Arrays/Maximum Count of Positive Integer and Negative Integer.py
+++ Arrays/Maximum Count of Positive Integer and Negative Integer.py
@@ -1,1 +1,10 @@
-
+class Solution(object):
+    def maximumCount(self, nums):
+        count=0
+        neg=0
+        for i in range(len(nums)):
+            if nums[i]>=1:
+                count+=1
+            elif nums[i]<0:
+                neg+=1
+        return max(count,neg)
