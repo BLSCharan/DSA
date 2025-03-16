24d48a58c99eff4cb472f5b125e0c28060cb2590
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-03-16 18:51:16 +0530

    Update Minimum Depth of Binary Tree.py

--- Trees/Minimum Depth of Binary Tree.py
+++ Trees/Minimum Depth of Binary Tree.py
@@ -1,1 +1,10 @@
+class Solution(object):
+    def minDepth(self, root):
+        if not root:
+            return 0
+        if not root.left:
+            return self.minDepth(root.right)+1
+        if not root.right:
+            return self.minDepth(root.left)+1
 
+        return min(self.minDepth(root.left),self.minDepth(root.right))+1
