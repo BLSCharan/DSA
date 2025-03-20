ea943449670ef8002cbc93e1257e67b725ba5e8f
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-03-20 09:16:38 +0530

    Update Merge Two Binary Trees.py

--- Trees/Merge Two Binary Trees.py
+++ Trees/Merge Two Binary Trees.py
@@ -1,1 +1,9 @@
-
+class Solution(object):
+    def mergeTrees(self, root1, root2):
+        if root1 and root2:
+            root = TreeNode(root1.val + root2.val)
+            root.left = self.mergeTrees(root1.left, root2.left)
+            root.right = self.mergeTrees(root1.right, root2.right)
+            return root
+        else:
+            return root1 or root2
