4b80c33e92830f61cecbfc1171b009279911524f
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-03-24 21:36:34 +0530

    Update Check Balanced String.py

--- Strings/Check Balanced String.py
+++ Strings/Check Balanced String.py
@@ -1,1 +1,12 @@
+class Solution(object):
+    def isBalanced(self, num):
+        e = 0  
+        o = 0  
+        for i in range(len(num)):
+            digit = int(num[i]) 
+            if i % 2 == 0:
+                e += digit  
+            else:
+                o += digit  
+        return e == o
 
