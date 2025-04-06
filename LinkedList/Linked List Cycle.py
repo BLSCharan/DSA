5ad2260588170ff2c84133e7f1660384deafb567
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-04-06 23:25:04 +0530

    Update Linked List Cycle.py

--- LinkedList/Linked List Cycle.py
+++ LinkedList/Linked List Cycle.py
@@ -1,1 +1,11 @@
-
+class Solution(object):
+    def hasCycle(self, head):
+        if head is None:
+            return 
+        fast = slow = head 
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
